0aef12d7313367ead4e2d8dd6d76e37c3b310888
silverism <silverism.oath@gmail.com>
2016-06-22 13:19:51 +0700

    Fix }

--- app/src/main/java/th/co/rcmo/rcmoapp/API/ResponseAPI.java
+++ app/src/main/java/th/co/rcmo/rcmoapp/API/ResponseAPI.java
@@ -39,8 +39,10 @@ public class ResponseAPI {
 
     public interface OnCallbackAPIListener {
         void callbackSuccess(Object obj);
+
         void callbackError(int code, String errorMsg);
     }
+
     public ResponseAPI(Context c) {
         this.c = c;
     }
@@ -51,75 +53,80 @@ public class ResponseAPI {
         aq = new AQuery(c);
     }
 
-    public void API_Request(final boolean ShowErrorcase,final String url) {
-        Log.i("", "API_Request "+url);
+    public void API_Request(final boolean ShowErrorcase, final String url) {
+        Log.i("", "API_Request " + url);
         aq.ajax(url,
                 String.class, new AjaxCallback<String>() {
 
                     @Override
                     public void callback(String url_ws, String html, AjaxStatus status) {
 
-                        Log.i(RequestServices.TAG, "API_Request url : " +url_ws);
-                        Log.i(RequestServices.TAG, "API_Request Response : " +html);
-                        if(html!= null) {
+                        Log.i(RequestServices.TAG, "API_Request url : " + url_ws);
+                        Log.i(RequestServices.TAG, "API_Request Response : " + html);
+                        if (html != null) {
 
-                        mStatus _status = new Gson().fromJson(html, mStatus.class);
-                        if(_status !=null){
-                            Log.i(RequestServices.TAG, "API_Request status : " + _status.getRespStatus().toString());
-                            if (status.getCode()==200) {
-                                if (_status.getRespStatus().getStatusID()==0) {
-                                    Object object = null;
-                                    if(url_ws.contains(RequestServices.ws_chkLogin)){
-                                        object = new Gson().fromJson(html, mLogin.class);
-                                    } else if (url_ws.contains(RequestServices.ws_saveRegister)) {
-                                        object = new Gson().fromJson(html, mRegister.class);
-                                    }else if (url_ws.contains(RequestServices.ws_getPlotList)) {
-                                        object = new Gson().fromJson(html, mUserPlotList.class);
-                                    }else if (url_ws.contains(RequestServices.ws_getProduct)) {
-                                        object = new Gson().fromJson(html, mProduct.class);
-                                    }else if (url_ws.contains(RequestServices.ws_getRiceProduct)) {
-                                        object = new Gson().fromJson(html, mRiceProduct.class);
-                                    }else if (url_ws.contains(RequestServices.ws_getPlantGroup)) {
-                                        object = new Gson().fromJson(html, mPlantGroup.class);
-                                    }else if (url_ws.contains(RequestServices.ws_getRegister)) {
-                                        object = new Gson().fromJson(html, mGetRegister.class);
-                                    }else if (url_ws.contains(RequestServices.ws_savePlotDetail)) {
-                                        object = new Gson().fromJson(html, mSavePlotDetail.class);
-                                    }else if (url_ws.contains(RequestServices.ws_deletePlot)) {
-                                        object = new Gson().fromJson(html, mDeletePlot.class);
-                                    }else if (url_ws.contains(RequestServices.ws_updateUserPlotSeq)) {
-                                        object = new Gson().fromJson(html, mUpdateUserPlotSeq.class);
-                                    }else if (url_ws.contains(RequestServices.ws_getProvince)) {
-                                        object = new Gson().fromJson(html, mProvince.class);
-                                    }else if (url_ws.contains(RequestServices.ws_getAmphoe)) {
-                                        object = new Gson().fromJson(html, mAmphoe.class);
-                                    }else if (url_ws.contains(RequestServices.ws_getTambon)) {
-                                        object = new Gson().fromJson(html, mTumbon.class);
-                                    }else if (url_ws.contains(RequestServices.ws_copyPlot)) {
-                                        object = new Gson().fromJson(html, mCopyPlot.class);
-                                    }else if (url_ws.contains(RequestServices.ws_getPlotDetail)) {
-                                        object = new Gson().fromJson(html, mGetPlotDetail.class);
-                                    }else if (url_ws.contains(RequestServices.ws_getVariable)) {
-                                        object = new Gson().fromJson(html, mGetVariable.class);
-                                    }else if (url_ws.contains(RequestServices.ws_getPlotSuit)) {
-                                        object = new Gson().fromJson(html, mGetPlotSuit.class);
+                            mStatus _status = new Gson().fromJson(html, mStatus.class);
+                            if (_status != null) {
+                                Log.i(RequestServices.TAG, "API_Request status : " + _status.getRespStatus().toString());
+                                if (status.getCode() == 200) {
+                                    if (_status.getRespStatus().getStatusID() == 0) {
+                                        Object object = null;
+                                        if (url_ws.contains(RequestServices.ws_chkLogin)) {
+                                            object = new Gson().fromJson(html, mLogin.class);
+                                        } else if (url_ws.contains(RequestServices.ws_saveRegister)) {
+                                            object = new Gson().fromJson(html, mRegister.class);
+                                        } else if (url_ws.contains(RequestServices.ws_getPlotList)) {
+                                            object = new Gson().fromJson(html, mUserPlotList.class);
+                                        } else if (url_ws.contains(RequestServices.ws_getProduct)) {
+                                            object = new Gson().fromJson(html, mProduct.class);
+                                        } else if (url_ws.contains(RequestServices.ws_getRiceProduct)) {
+                                            object = new Gson().fromJson(html, mRiceProduct.class);
+                                        } else if (url_ws.contains(RequestServices.ws_getPlantGroup)) {
+                                            object = new Gson().fromJson(html, mPlantGroup.class);
+                                        } else if (url_ws.contains(RequestServices.ws_getRegister)) {
+                                            object = new Gson().fromJson(html, mGetRegister.class);
+                                        } else if (url_ws.contains(RequestServices.ws_savePlotDetail)) {
+                                            object = new Gson().fromJson(html, mSavePlotDetail.class);
+                                        } else if (url_ws.contains(RequestServices.ws_deletePlot)) {
+                                            object = new Gson().fromJson(html, mDeletePlot.class);
+                                        } else if (url_ws.contains(RequestServices.ws_updateUserPlotSeq)) {
+                                            object = new Gson().fromJson(html, mUpdateUserPlotSeq.class);
+                                        } else if (url_ws.contains(RequestServices.ws_getProvince)) {
+                                            object = new Gson().fromJson(html, mProvince.class);
+                                        } else if (url_ws.contains(RequestServices.ws_getAmphoe)) {
+                                            object = new Gson().fromJson(html, mAmphoe.class);
+                                        } else if (url_ws.contains(RequestServices.ws_getTambon)) {
+                                            object = new Gson().fromJson(html, mTumbon.class);
+                                        } else if (url_ws.contains(RequestServices.ws_copyPlot)) {
+                                            object = new Gson().fromJson(html, mCopyPlot.class);
+                                        } else if (url_ws.contains(RequestServices.ws_getPlotDetail)) {
+                                            object = new Gson().fromJson(html, mGetPlotDetail.class);
+                                        } else if (url_ws.contains(RequestServices.ws_getVariable)) {
+                                            object = new Gson().fromJson(html, mGetVariable.class);
+                                        } else if (url_ws.contains(RequestServices.ws_getPlotSuit)) {
+                                            object = new Gson().fromJson(html, mGetPlotSuit.class);
+                                        }
+                                        onCallbackAPIListener.callbackSuccess(object);
+                                    } else {
+                                        if (ShowErrorcase)
+                                            new DialogChoice(c).ShowOneChoice("", _status.getRespStatus().getStatusMsg());
+                                        else
+                                            onCallbackAPIListener.callbackError(_status.getRespStatus().getStatusID()
+                                                    , _status.getRespStatus().getStatusMsg());
                                     }
-                                    onCallbackAPIListener.callbackSuccess(object);
                                 } else {
-                                    if(ShowErrorcase)
-                                        new DialogChoice(c).ShowOneChoice("",_status.getRespStatus().getStatusMsg());
-                                    else onCallbackAPIListener.callbackError(_status.getRespStatus().getStatusID()
-                                            ,_status.getRespStatus().getStatusMsg());
+                                    new DialogChoice(c).ShowOneChoice("", "กรุณาเชื่อมต่ออินเตอร์เน็ต");
                                 }
-                            }else {
-                                new DialogChoice(c).ShowOneChoice("","กรุณาเชื่อมต่ออินเตอร์เน็ต");
+                            } else {
+                                new DialogChoice(c).ShowOneChoice("", "การเชื่อมต่อ Server ผิดพลาด");
                             }
-                        }else{
-                            new DialogChoice(c).ShowOneChoice("", "การเชื่อมต่อ Server ผิดพลาด");
+
                         }
 
                     }
 
-                });
+
+                }
+        );
     }
 }
