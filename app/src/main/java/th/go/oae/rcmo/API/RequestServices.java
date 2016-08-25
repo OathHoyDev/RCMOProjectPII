25537ce7d3477711db8dfc87403813212a1b53a9
taweesin <taweesin2014@outlook.com>
2016-08-25 15:15:51 +0700

    post prod 4 By Taweesin

--- app/src/main/java/th/go/oae/rcmo/API/RequestServices.java
+++ app/src/main/java/th/go/oae/rcmo/API/RequestServices.java
@@ -6,7 +6,9 @@ package th.go.oae.rcmo.API;
 public class RequestServices {
     public static String TAG = "RequestSurvices";
 
-    public static String url = "http://111.223.34.154/WS_OAE1603/services.asmx/";
+   // public static String url = "http://111.223.34.154/WS_OAE1603/services.asmx/";
+    public static String url = "http://111.223.34.154/WS_RCMO/services.asmx/";
+
     public static String ws_chkLogin = url+"chkLogin";
     public static String ws_saveRegister = url+"saveRegister";
     public static String ws_getPlotList = url+"getPlotList";
